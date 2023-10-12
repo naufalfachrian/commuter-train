b5424377b2d4ec5e41c363fb9e2ce71387387ac9
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-10-12 14:14:04 +0700

    Initial Commit

---
+++ CommuterTrain/CommuterTrainApp.swift
@@ -0,0 +1,20 @@
+//
+//  CommuterTrainApp.swift
+//  CommuterTrain
+//
+//  Created by Bunga Mungil on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CommuterTrainApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
