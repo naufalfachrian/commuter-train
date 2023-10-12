bb06135ad74c9c23f5e8fae82148d8ce1738cfe3
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-10-12 15:57:09 +0700

    Implement route information

---
+++ CommuterTrain/CommuterLine.swift
@@ -0,0 +1,14 @@
+//
+//  CommuterLine.swift
+//  CommuterTrain
+//
+//  Created by Bunga Mungil on 12/10/23.
+//
+
+import SwiftUI
+
+struct CommuterLine: Identifiable {
+    let id = UUID()
+    let color: Color
+    let name: String
+}
--- CommuterTrain/RouteInformationView.swift
+++ CommuterTrain/RouteInformationView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct RouteInformationView: View {
+    @State var sideBarVisibility: NavigationSplitViewVisibility = .all
+    
+    let commuterLines: [CommuterLine] = [
+        CommuterLine(color: .blue, name: "Cikarang Line"),
+        CommuterLine(color: .red, name: "Bogor Line"),
+        CommuterLine(color: .pink, name: "Tanjungpriok Line"),
+        CommuterLine(color: .green, name: "Rangkasbitung Line"),
+        CommuterLine(color: .orange, name: "Tangerang Line"),
+        CommuterLine(color: .red, name: "Yogyakarta Line")
+    ]
+    
     var body: some View {
-        Text("Route Information")
+        NavigationStack {
+            List(self.commuterLines) { line in
+                NavigationLink {
+                    Text(line.name).foregroundColor(line.color)
+                        .navigationTitle(line.name)
+                } label: {
+                    Text(line.name).foregroundColor(line.color)
+                }
+            }
+            .navigationTitle("Route Information")
+        }
     }
 }
 
