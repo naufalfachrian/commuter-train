cb1862008c2df5feafe5d7367cac43e6ddffa9c3
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-10-12 16:46:07 +0700

    Implement train schedule

--- CommuterTrain/TrainScheduleView.swift
+++ CommuterTrain/TrainScheduleView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct TrainScheduleView: View {
+    private static let stations = [
+        "Kalideres",
+        "Pesing"
+    ]
+    
+    @State private var selectedStation = TrainScheduleView.stations[0]
+    
     var body: some View {
-        Text("Train Schedule")
+        NavigationStack {
+            List {
+                Section {
+                    Picker(selection: $selectedStation, label: Text("Choose Station")) {
+                        ForEach(TrainScheduleView.stations, id: \.self) { stationName in
+                            Text(stationName)
+                        }
+                    }
+                    .tint(CommuterTrainApp.accentColor)
+                    #if os(iOS)
+                    .pickerStyle(.navigationLink)
+                    #endif
+                    #if os(macOS)
+                    .pickerStyle(.menu)
+                    #endif
+                    DatePicker(selection: .constant(Date()), displayedComponents: .hourAndMinute, label: { Text("From") })
+                    DatePicker(selection: .constant(Date().addingTimeInterval(3600)), displayedComponents: .hourAndMinute, label: { Text("To") })
+                }
+            }
+            .navigationTitle("Train Schedule")
+        }
     }
 }
 
